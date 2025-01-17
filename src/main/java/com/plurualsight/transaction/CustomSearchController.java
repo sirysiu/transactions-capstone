3691c1c97e91dd11cd9e41cc109a340c6aefbbc9
Jacob Lockhart <procoder165@gmail.com>
2025-01-17 10:42:28 -0500

    created customer search controller and filled searching

--- src/main/java/com/plurualsight/transaction/CustomSearchController.java
+++ src/main/java/com/plurualsight/transaction/CustomSearchController.java
@@ -1,25 +1,31 @@
 package com.plurualsight.transaction;
 
-import javax.swing.tree.TreeNode;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CustomSearchController {
-    Scanner scan = new Scanner(System.in);
     ArrayList<Transaction> ledger;
 
     public CustomSearchController(ArrayList<Transaction> ledger) {
         this.ledger = ledger;
     }
 
-    public void search(String vendor,String startDate, String endDate, String description, float decimal ) {
-
+    public ArrayList<Transaction> search(String vendor, String startDate, String endDate, String description, double amount ) {
+        ArrayList<Transaction> newLedger = new ArrayList<>();
         /*
         vendor
         date
         description
         amount
          */
-        ledger.stream().filter(t -> vendor == null || t.getVendor().contains(vendor));
+        ledger.stream().filter(t -> vendor == null || t.getVendor().contains(vendor))
+                .filter((t -> startDate == null || LocalDate.parse(t.getDate()).isAfter(LocalDate.parse(startDate))))
+                .filter(t -> endDate == null || LocalDate.parse(t.getDate()).isBefore(LocalDate.parse(endDate)))
+                .filter(t -> description == null || t.getDescription().contains(description))
+                .filter(t-> amount == 0.0 || t.getAmount() == amount)
+                .forEach(t -> newLedger.add(t));
+
+        return newLedger;
     }
 }
